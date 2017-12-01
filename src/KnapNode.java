f95ca21e3077728b1be3ad9e9521105655b802e1
imerrill9 <ian.merrill9@gmail.com>
2017-12-01 13:09:57 -0700

    Starting framework

--- src/KnapNode.java
+++ src/KnapNode.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 /**
- * Tour is the node of the KnapTree which must calculate it's weight and cost from a list of considered items.
+ * KnapNode is the node of the KnapTree which must calculate it's weight and cost from a list of considered items.
  */
-public class Tour
+public class KnapNode
 {
 	private ArrayList<Item> itemList;    //The item list being considered by the node
 	private int weight;                  //Weight of tourList this node considers
@@ -12,10 +12,10 @@ public class Tour
 	private boolean prune;               //Should the node be pruned?
 
 	//node attributes
-	private Tour leftT;
-	private Tour rightT;
+	private KnapNode leftT;
+	private KnapNode rightT;
 
-	public Tour(ArrayList<Item> items, int weight, int cost)
+	public KnapNode(ArrayList<Item> items, int weight, int cost)
 	{
 		this.itemList = items;
 		calculateWeight(items);
@@ -26,9 +26,9 @@ public class Tour
 	/**
 	 * Calculate the value of weight
 	 *
-	 * @param tourList tour of nodes considered
+	 * @param items list of items being considered by this node
 	 */
-	private void calculateWeight(ArrayList<Item> tourList)
+	private void calculateWeight(ArrayList<Item> items)
 	{
 
 	}
@@ -36,9 +36,9 @@ public class Tour
 	/**
 	 * Calculate the value of cost
 	 *
-	 * @param tourList tour of nodes considered
+	 * @param items list of items being considered by this node
 	 */
-	private void calculateCost(ArrayList<Item> tourList)
+	private void calculateCost(ArrayList<Item> items)
 	{
 
 	}
@@ -46,9 +46,9 @@ public class Tour
 	/**
 	 * Calculate the potential cost using fractions of costs
 	 *
-	 * @param tourList tour of nodes considered
+	 * @param items list of items being considered by this node
 	 */
-	private void calculatePotentialCost(ArrayList<Item> tourList)
+	private void calculatePotentialCost(ArrayList<Item> items)
 	{
 
 	}
--- src/KnapTree.java
+++ src/KnapTree.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
  */
 public class KnapTree
 {
-	public ArrayList<Tour> leaves;    // leaves to consider returning to
+	public ArrayList<KnapNode> leaves;    // leaves to consider returning to
 	public ArrayList<Item> items;     // items to choose from
 	public int capacity;              // capacity of the Knapsack
 
-	private Tour head;
+	private KnapNode head;
 
 	public KnapTree(ArrayList<Item> items, int capacity)
 	{
 
 		this.capacity = capacity;
 		this.items = items;
-		head = new Tour(null, 0, 0);
+		head = new KnapNode(null, 0, 0);
 	}
 
 
